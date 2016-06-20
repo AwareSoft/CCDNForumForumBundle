e62b791e512419ffa1883b3efcaddbf83d0d9fc0
Bartosz Malec <b.malec@awaresoft.pl>
2016-06-20 15:18:17 +0200

    fixed problem with manually added class namespace in subscription manager

--- Model/Component/Manager/SubscriptionManager.php
+++ Model/Component/Manager/SubscriptionManager.php
@@ -56,7 +56,8 @@ class SubscriptionManager extends BaseManager implements ManagerInterface
         $subscription = $this->model->findOneSubscriptionForTopicByIdAndUserById($topic->getId(), $user->getId());
 
         if (! $subscription) {
-            $subscription = new Subscription();
+            $class = $this->getGateway()->getEntityClass();
+            $subscription = new $class;
         }
 
         if (! $subscription->isSubscribed()) {
